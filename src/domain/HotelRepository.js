e8be07a3a89f793f14b52aca5d64c2cd32d25a2c
hisanari <hisanari.1987@gmail.com>
2017-11-11 09:57:47 +0900

    add distance

--- src/domain/HotelRepository.js
+++ src/domain/HotelRepository.js
@@ -1,3 +1,5 @@
+import geolib from 'geolib';
+
 import Rakuten from '../lib/Rakuten';
 
 const RAKUTEN_APP_ID = '1010940367941491853';
@@ -14,10 +16,20 @@ export const searchHotelByLocation = (location) => {
     .then(result =>
       result.data.hotels.map((hotel) => {
         const basicInfo = hotel.hotel[0].hotelBasicInfo;
+        const price = basicInfo.hotelMinCharge;
+        const distance = geolib.getDistance(
+          { latitude: location.lat, longitude: location.lng },
+          { latitude: basicInfo.latitude, longitude: basicInfo.longitude },
+        );
         return {
           id: basicInfo.hotelNo,
           name: basicInfo.hotelName,
           url: basicInfo.hotelInformationUrl,
+          thumUrl: basicInfo.hotelThumbnailUrl,
+          price: price ? `${price}円` : '空室なし',
+          reviewAverage: basicInfo.reviewAverage,
+          reviewCount: basicInfo.reviewCount,
+          distance,
         };
       }),
     );
